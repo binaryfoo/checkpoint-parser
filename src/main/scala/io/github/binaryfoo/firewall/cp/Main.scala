496e4a7fc0da16aa632fb46fda5d6c3760e55105
Will Currie <will@currie.id.au>
2016-02-27 10:26:59 +1100

    Read from file too

--- src/main/scala/io/github/binaryfoo/firewall/cp/Main.scala
+++ src/main/scala/io/github/binaryfoo/firewall/cp/Main.scala
@@ -5,7 +5,11 @@ import scala.io.Source
 object Main {
 
   def main(args: Array[String]) {
-    val source = Source.fromInputStream(System.in)
+    val source = if (args.length == 1) {
+      Source.fromFile(args(0))
+    } else {
+      Source.fromInputStream(System.in)
+    }
     val exported = Extractor.extractRules(source)
     val rules = RuleParser.parse(exported)
     println(Jsonifier.toJson(rules))
